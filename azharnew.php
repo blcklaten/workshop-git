08e5caa6e9ef9f52ff4b2095eb869298ecefbbee
Nasrulloh Azhar <archknightx@gmail.com>
2017-06-14 23:59:28 +0700

    azharnew.php

---
+++ azharnew.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Malem Gan!<br>";
+echo "Saya Newbie PHP!<br>";
+echo "Iki ", "Digawe ", "Pas ", "Ngantuk ", "Nang BLC.";
+?> 
+
+</body>
+</html>
