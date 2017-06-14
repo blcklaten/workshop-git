fb13f1be353aa890bb24dad6e275706a5d00c276
yudhabernaditus <yudhabernaditus@gmail.com>
2017-06-14 23:42:06 +0700

    RUMUS

---
+++ Rumus.py
@@ -0,0 +1,9 @@
+def segitiga () :
+
+	print ("Luas segitiga")
+	print ("*************")
+	a = float(input("Masukan alas	: "))
+	t = float(input("Masukan tinngi : "))
+	luas= 0.5*a*t
+	print ("luas segitiga =" ,luas, "cm2")
+Segitiga ()
