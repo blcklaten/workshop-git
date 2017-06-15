26033f084f1f6207431025c182be4f7b4b242c1c
nazzhar <archknightx@gmail.com>
2017-06-15 08:48:13 +0700

    Create azharnew.php

--- azharnew.php
+++ azharnew.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "<h2>PHP is Fun!</h2>";
+echo "<h2>My Name is Nasrulloh Azhar</h2>";
 echo "Malem Gan!<br>";
 echo "Saya Newbie PHP!<br>";
 echo "Iki ", "Digawe ", "Pas ", "Ngantuk ", "Nang BLC.";
